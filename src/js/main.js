5516b884d6bc5d3a8776a95ac2c43698f7ff38d5
Serednii Mykola <seredniimykola@gmail.com>
2023-12-30 23:12:20 +0100

    add style section programs and modue rater1

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,14 @@
 // import ButtonComponent from '@/components/ButtonComponent'
 import './components/burger'
 
-// import Swiper from 'swiper'
-// import 'swiper/css'
-
 import $ from 'jquery'
+import Swiper from 'swiper'
+
 import 'slick-slider'
+
+const rater = require('rater-js')
+
+// import '../../node_modules/star-rating-svg/dist/jquery.star-rating-svg'
 
 // const button = new ButtonComponent('.button')
 
@@ -18,12 +21,12 @@ import 'slick-slider'
 
 // var swiperContainer = document.querySelector('.slider');
 
-//swiper має дві обгортки swiper є зовнішньою а swiper-wrapper обгортка з елементами swiper-slide    https://swiperjs.com/get-started
-// const swiper = new Swiper('.swiper', {
-//   pagination: {
-//     el: ".swiper-pagination",
-//   },
-// });
+// swiper має дві обгортки swiper є зовнішньою а swiper - wrapper обгортка з елементами swiper - slide    https://swiperjs.com/get-started
+new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+})
 
 $('.slider').slick({
   infinite: true,
@@ -52,3 +55,34 @@ $('.slider').slick({
     },
   ],
 })
+
+const ratingElements = document.querySelectorAll('.rater')
+function raterRouter(el, ratingInput) {
+  const myRater = rater({
+    max: 5,
+    rating: ratingInput,
+    element: el,
+    rateCallback: function rateCallback(rating, done) {
+      myDataService.rate(rate).then(
+        avgRating => {
+          // update the avarage rating with the one we get from the server
+          myRater.setRating(avgRating)
+          // we could disable the rater to prevent another rating
+          // if we dont want the user to be able to change their mind
+          myRater.disable()
+          // dont forget to call done
+          done()
+        },
+        () => {
+          // handle the error
+          // dont forget to call done
+          done()
+        },
+      )
+    },
+  })
+}
+
+ratingElements.forEach(el =>
+  raterRouter(el, Number(el.previousElementSibling.dataset.rating)),
+)
